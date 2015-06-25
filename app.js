300f410ebde97f2e9259c27f30717e468f08804d
Wen Li <wenli936@gmail.com>
2015-06-25 00:00:45 -0700

    added bootstrap styles

--- app.js
+++ app.js
@@ -6,10 +6,28 @@ app.controller('MainCtrl', [
 	  $scope.test = 'Hello world!';	
 
 		$scope.posts = [
-			'post 1',
-			'post 2',
-			'post 3',
-			'post 4',
-			'post 5'
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2},
+			{title: 'post 3', upvotes: 15},
+			{title: 'post 4', upvotes: 9},
+			{title: 'post 5', upvotes: 4}
 		];
+
+		$scope.addPost = function(){
+			if (!$scope.title || $scope.title === '') { return; }
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0
+			});
+			
+			$scope.title = '';
+			$scope.link = '';
+		}
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
+
 	}]);
+
