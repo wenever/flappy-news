9c191c2550c3ab412cc831695ce264b7586b09fe
Wen Li <wenli936@gmail.com>
2015-06-25 02:28:41 -0700

    added adding comment feature

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ app.controller('PostsCtrl', [
 'posts',
 function($scope, $stateParams, posts) {
 	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+		if ($scope.body === '') { return; }
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+
 }]);
 
 app.config([
