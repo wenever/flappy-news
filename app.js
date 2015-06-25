043abcc2b83aabdd9fdb9a311f61e500b5ba569e
Wen Li <wenli936@gmail.com>
2015-06-24 23:18:16 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var app = angular.module('flappyNews', []);
+
+app.controller('MainCtrl', [
+	'$scope',
+	function($scope){
+	  $scope.test = 'Hello world!';	
+
+		$scope.posts = [
+			'post 1',
+			'post 2',
+			'post 3',
+			'post 4',
+			'post 5'
+		];
+	}]);
