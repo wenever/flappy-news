e422fa334b7d328c8f6cab8dc519e29d6d4e6310
Wen Li <wenli936@gmail.com>
2015-06-25 01:45:51 -0700

    added separate posts ctrl and routing for viewing comments

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var app = angular.module('flappyNews', []);
+var app = angular.module('flappyNews', ['ui.router']);
+
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
 
 app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
+	'posts',
+	function($scope, posts){
 	  $scope.test = 'Hello world!';	
 
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		];
+		$scope.posts = posts.posts;
 
 		$scope.addPost = function(){
 			if (!$scope.title || $scope.title === '') { return; }
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				//fake comments for now
+				comments: [
+					{author: 'Joe', body: 'Sloppy!', upvotes: 0},
+					{author: 'Bob', body: 'Where is Alice?', upvotes: 0}
+				]
 			});
 			
 			$scope.title = '';
@@ -29,5 +36,42 @@ app.controller('MainCtrl', [
 			post.upvotes += 1;
 		};
 
-	}]);
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+
+}]);
+
+
+
+
+
+
+
+
 
